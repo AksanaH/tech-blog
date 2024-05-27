d52995e1990016bb87ecfcec1274350a6776fd2d
Aksana <aksa.hleb@gmail.com>
2024-05-27 18:12:17 -0500

    Add login/logout button functionality

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -26,5 +26,16 @@ router.get('/', async (req, res) => {
         res.status(500).json(err);
     }
 });
+router.get('/login', (req, res) => {
+    // If the user is already logged in, redirect the request to another route
+    console.log("before")
+    if (req.session.logged_in) {
+        res.redirect('/');
+        return;
+    }
+    console.log("after")
+
+    res.render('login');
+});
 
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 
-// const apiRoutes = require('./api');
+const userRoutes = require('./userRoutes');
 const homeRoutes = require('./homeRoutes');
 
 router.use('/', homeRoutes);
-// router.use('/api', apiRoutes);
+router.use('/users', userRoutes);
 
 module.exports = router;
